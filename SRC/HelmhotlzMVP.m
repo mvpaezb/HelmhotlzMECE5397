c012e98448a15fe5109b85b34032f79c692d6858
mvpaezb <mvpaezb@gmail.com>
2017-05-03 14:54:17 -0500

    Code -Trial 2

--- SRC/HelmhotlzMVP.m
+++ SRC/HelmhotlzMVP.m
@@ -3,7 +3,7 @@
 clc
 clear all 
 %Ghost nodes, made as input in order to manipulate the code
-n=input('Enter your value for n')
+n=input('Enter your value for n=')
 %Given values, constants
 gamma=-1; ax=-pi; ay=-pi; by=pi; bx=pi;
 %Creating vector with linespace function
@@ -14,12 +14,15 @@ u(:,1)=ax;
 u(:,n)=((bx-ax).^2.*cos((pi.*bx)./ax))+((y(:)-ay)./(by-ay)).*(bx.*(bx-ax).^2-((bx-ax).^2.*cos((pi.*bx)./ax)));
 u(1,:)=x(:).*(x(:)-ax).^2;
 u(n,:)=(x(:)-ax).^2.*cos(pi.*x(:)./ax);
-h=length(x)/n
+h=length(x)/n;
+it=1000;
 
+for k=1:it
 for  j=2:n-1
     for i=2:n-1
         F=sin(pi.*((x(i)-ax)/(bx-ax))).*cos((pi/2).*(2.*(((y(j)-ay)/(by-ay))+1)));
-        u(i,j)= 1/(4-gamma)*(u(i-1,j)+u(i+1,j)+u(i,j-1)+u(i,j+1))*(1/h);
+        u(i,j)= 1/(4).*(u(i-1,j)+u(i+1,j)+u(i,j-1)+u(i,j+1)).*(1/h);
     end 
+end
 end
 surf(x,y,u)
