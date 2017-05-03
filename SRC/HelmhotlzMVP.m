79d7bd5eea0b1b5c99fd850aa02e098043f748a0
mvpaezb <mvpaezb@gmail.com>
2017-05-03 16:00:06 -0500

    Updates - Code Trial 4

--- SRC/HelmhotlzMVP.m
+++ SRC/HelmhotlzMVP.m
@@ -1,20 +1,29 @@
+%Scientific Computing MECE 5397
 %Maria Violeta Paez 
-%Helmholtz Equation - MECE 5397
+%Project A - Helmholtz Equation
+%Helmholtz Equation using Gauss-Seidel Method 
+
 clc
 clear all 
+
 %Ghost nodes, made as input in order to manipulate the code
 n=input('Enter your value for n= ')
+
 %Given values, constants
+
 gamma=-1; ax=-pi; ay=-pi; by=pi; bx=pi;
+
 %Creating vector with linespace function
 x=linspace(ax,bx,n); y=linspace(ay,by,n);
+
 %Boundary conditions
 u=zeros(n);
 u(:,1)=ax;
 u(:,n)=((bx-ax).^2.*cos((pi.*bx)./ax))+((y(:)-ay)./(by-ay)).*(bx.*(bx-ax).^2-((bx-ax).^2.*cos((pi.*bx)./ax)));
 u(1,:)=x(:).*(x(:)-ax).^2;
 u(n,:)=(x(:)-ax).^2.*cos(pi.*x(:)./ax);
-h=length(x)/n;
+
+h=bx/n; %Step Size 
 it=1000;
 
 for k=1:it
@@ -24,6 +33,7 @@ for  j=2:n-1
         %Discritization 
         u(i,j)= 1/(4).*(u(i-1,j)+u(i+1,j)+u(i,j-1)+u(i,j+1)+F(i,j).*h.^2);
     end 
+    u(i,n)= 1/(4).*(u(i-1,j)+u(i-1,j)+u(i,j-1)+u(i,j+1)+F(i,j).*h.^2);
 end
 end
 %Plot 
