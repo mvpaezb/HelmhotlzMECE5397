4b6524c2666d3ba2b272f6abaedd7fdf20ed1c9d
mvpaezb <mvpaezb@gmail.com>
2017-05-03 12:42:59 -0500

    Code - Trial 1

--- SRC/HelmhotlzMVP.m
+++ SRC/HelmhotlzMVP.m
@@ -14,8 +14,12 @@ u(:,1)=ax;
 u(:,n)=((bx-ax).^2.*cos((pi.*bx)./ax))+((y(:)-ay)./(by-ay)).*(bx.*(bx-ax).^2-((bx-ax).^2.*cos((pi.*bx)./ax)));
 u(1,:)=x(:).*(x(:)-ax).^2;
 u(n,:)=(x(:)-ax).^2.*cos(pi.*x(:)./ax);
+h=length(x)/n
 
-
-
-
+for  j=2:n-1
+    for i=2:n-1
+        F=sin(pi.*((x(i)-ax)/(bx-ax))).*cos((pi/2).*(2.*(((y(j)-ay)/(by-ay))+1)));
+        u(i,j)= 1/(4-gamma)*(u(i-1,j)+u(i+1,j)+u(i,j-1)+u(i,j+1))*(1/h);
+    end 
+end
 surf(x,y,u)
